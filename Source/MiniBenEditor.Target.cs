6b68a472153754c98f731202f7e842c1651f22ec
kirill.yakubov <kirill.yakubov@deuzspark.com>
2024-10-19 17:21:29 -0400

    First Commit from local

---
+++ Source/MiniBenEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class MiniBenEditorTarget : TargetRules
+{
+	public MiniBenEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		ExtraModuleNames.Add("MiniBen");
+	}
+}
