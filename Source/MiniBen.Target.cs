9c0ad39d00c1a30a1a1ef81c5a6ef2899bccd88c
kirill.yakubov <kirill.yakubov@deuzspark.com>
2024-10-19 18:02:08 -0400

    Adding Player Actions Module

--- Source/MiniBen.Target.cs
+++ Source/MiniBen.Target.cs
@@ -11,5 +11,6 @@ public class MiniBenTarget : TargetRules
 		DefaultBuildSettings = BuildSettingsVersion.V5;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("MiniBen");
-	}
+        ExtraModuleNames.Add("PlayerActions");
+    }
 }
--- Source/MiniBenEditor.Target.cs
+++ Source/MiniBenEditor.Target.cs
@@ -11,5 +11,6 @@ public class MiniBenEditorTarget : TargetRules
 		DefaultBuildSettings = BuildSettingsVersion.V5;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		ExtraModuleNames.Add("MiniBen");
-	}
+        ExtraModuleNames.Add("PlayerActions");
+    }
 }
