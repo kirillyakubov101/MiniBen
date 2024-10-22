ee98cb6c0d71cd31e4c2bbc59200abae7dcf5502
kirill.yakubov <kirill.yakubov@deuzspark.com>
2024-10-22 00:22:09 -0400

    Add Streaming bridge test in native

--- Source/LevelStreamingUtils/Private/LevelStreamingBridge.cpp
+++ Source/LevelStreamingUtils/Private/LevelStreamingBridge.cpp
@@ -2,12 +2,14 @@
 
 
 #include "LevelStreamingBridge.h"
+#include "Engine/LevelStreaming.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 ALevelStreamingBridge::ALevelStreamingBridge()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
@@ -16,6 +18,33 @@ void ALevelStreamingBridge::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void ALevelStreamingBridge::ListAllSubLevels(UWorld* World) const
+{
+	UWorld* CurrentWorld = nullptr;
+	
+	if (World == nullptr)
+	{
+		CurrentWorld = GetWorld();
+	}
+	else
+	{
+		CurrentWorld = World;
+	}
+
+	const TArray<ULevelStreaming*>& ListOfStreamingLevels = CurrentWorld->GetStreamingLevels();
+
+	for (ULevelStreaming* StreamingLevel : ListOfStreamingLevels)
+	{
+		if (StreamingLevel)
+		{
+			FString LevelName = StreamingLevel->GetWorldAssetPackageName();
+			UE_LOG(LogTemp, Warning, TEXT("Sublevel: %s"), *LevelName);
+			bool bIsLevelLoaded = StreamingLevel->IsLevelLoaded();
+			UE_LOG(LogTemp, Warning, TEXT("Is level loaded: %s"), bIsLevelLoaded ? TEXT("true") : TEXT("false"));
+		}
+	}
 }
 
 // Called every frame
--- Source/LevelStreamingUtils/Public/LevelStreamingBridge.h
+++ Source/LevelStreamingUtils/Public/LevelStreamingBridge.h
@@ -19,6 +19,9 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UFUNCTION(BlueprintCallable)
+	void ListAllSubLevels(UWorld* World) const;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
