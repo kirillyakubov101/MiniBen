1f6edd46f49d72ab0f85d0ec1fb633bfb267371f
kirill.yakubov <kirill.yakubov@deuzspark.com>
2024-10-27 21:45:58 -0400

    Adding interaction system
    Adding collect item data asset; testing save

---
+++ Source/MiniBen/Public/CollectableitemsDataAsset.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CollectableitemsDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class MINIBEN_API UCollectableitemsDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+public:
+	// ItemType defines the category, such as "Collectable" or "Weapon"
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item")
+	FPrimaryAssetType ItemType = "Collectable";
+
+    // ItemID can be any descriptive identifier, like "healing potion" or "mana potion"
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item")
+    FName ItemID;
+
+    // Override the GetPrimaryAssetId function to return the asset ID based on ItemType and ItemID
+    virtual FPrimaryAssetId GetPrimaryAssetId() const override
+    {
+        return FPrimaryAssetId(ItemType, ItemID);
+    }
+};
