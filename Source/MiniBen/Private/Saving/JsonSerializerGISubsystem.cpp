13901df1f366188cd31e1be1c02c30198d49bc80
kirill.yakubov <kirillyakubov101@gmail.com>
2024-11-18 21:11:47 -0500

    Adding additional json serializations

--- Source/MiniBen/Private/Saving/JsonSerializerGISubsystem.cpp
+++ Source/MiniBen/Private/Saving/JsonSerializerGISubsystem.cpp
@@ -3,8 +3,102 @@
 
 #include "Saving/JsonSerializerGISubsystem.h"
 #include "MyStructs.h"
+#include "Serialization/JsonSerializer.h"
+#include "Serialization/JsonWriter.h"
 
 void UJsonSerializerGISubsystem::SerializeToJSON(const FMainSaveData& data)
 {
+	TSharedPtr<FJsonObject> JsonObject = MakeShared<FJsonObject>();
 
+	//Character
+	JsonObject->SetObjectField("CharacterStats", SerializeCharacterStats(data.PlayerStats)); 
+
+	  // Serialize AllLevels (TMap)
+	TSharedPtr<FJsonObject> LevelsObject = MakeShared<FJsonObject>();
+	for (const auto& Entry : data.AllLevels)
+	{
+		LevelsObject->SetObjectField(Entry.Key, SerializeWorldDataSave(Entry.Value));
+	}
+
+	JsonObject->SetObjectField(TEXT("AllLevels"), LevelsObject);
+	JsonObject->SetObjectField("Invetory", SerializePlayerInventory(data.PlayerInventory));
+
+	// Convert to JSON string
+	FString OutputString;
+	TSharedRef<TJsonWriter<>> JsonWriter = TJsonWriterFactory<>::Create(&OutputString);
+	FJsonSerializer::Serialize(JsonObject.ToSharedRef(), JsonWriter);
+
+	// Save to file
+	FFileHelper::SaveStringToFile(OutputString, *FilePath);
+}
+
+TSharedPtr<FJsonObject> UJsonSerializerGISubsystem::SerializeCharacterStats(const FCharacterStats& Stats)
+{
+	TSharedPtr<FJsonObject> JsonObject = MakeShared<FJsonObject>();
+
+	JsonObject->SetNumberField(TEXT("PlayerHealth"), Stats.PlayerHealth);
+	JsonObject->SetNumberField(TEXT("PlayerLevel"), Stats.PlayerLevel);
+	JsonObject->SetNumberField(TEXT("PlayerExp"), Stats.PlayerExp);
+
+
+	return JsonObject;
+}
+
+TSharedPtr<FJsonObject> UJsonSerializerGISubsystem::SerializeSaveableWorldItem(const FSaveableWorldItem& Item)
+{
+	TSharedPtr<FJsonObject> JsonObject = MakeShared<FJsonObject>();
+	JsonObject->SetBoolField(TEXT("ShouldBeRemoved"), Item.ShouldBeRemoved);
+	JsonObject->SetStringField(TEXT("Guid"), Item.Guid.ToString());
+
+	return JsonObject;
+}
+
+TSharedPtr<FJsonObject> UJsonSerializerGISubsystem::SerializeWorldDataSave(const FWorldDataSave& WorldData)
+{
+	TSharedPtr<FJsonObject> JsonObject = MakeShared<FJsonObject>();
+	JsonObject->SetBoolField(TEXT("bHasLevelBeenInitialized"), WorldData.bHasLevelBeenInitialized);
+
+	TSharedPtr<FJsonObject> SublevelsObject = MakeShared<FJsonObject>();
+
+	for (const auto& Entry : WorldData.ListOfSublevels)
+	{
+		SublevelsObject->SetBoolField(Entry.Key, Entry.Value);
+	}
+
+	JsonObject->SetObjectField(TEXT("ListOfSublevels"), SublevelsObject);
+
+	// Serialize PlayerPosition (FVector)
+	TSharedPtr<FJsonObject> PositionObject = MakeShared<FJsonObject>();
+	PositionObject->SetNumberField(TEXT("X"), WorldData.PlayerPosition.X);
+	PositionObject->SetNumberField(TEXT("Y"), WorldData.PlayerPosition.Y);
+	PositionObject->SetNumberField(TEXT("Z"), WorldData.PlayerPosition.Z);
+
+	JsonObject->SetObjectField(TEXT("PlayerPosition"), PositionObject);
+
+	// Serialize ListOfLevelAssets (TArray)
+	TArray<TSharedPtr<FJsonValue>> LevelAssetsArray;
+	for (const FSaveableWorldItem& Item : WorldData.ListOfLevelAssets)
+	{
+		LevelAssetsArray.Add(MakeShared<FJsonValueObject>(SerializeSaveableWorldItem(Item)));
+	}
+
+	JsonObject->SetArrayField(TEXT("ListOfLevelAssets"), LevelAssetsArray);
+
+
+	return JsonObject;
+}
+
+TSharedPtr<FJsonObject> UJsonSerializerGISubsystem::SerializePlayerInventory(const TMap<FName, int32>& WorldData)
+{
+	TSharedPtr<FJsonObject> JsonObject = MakeShared<FJsonObject>();
+	TSharedPtr<FJsonObject> PlayerInventoryObject = MakeShared<FJsonObject>();
+
+	for (const auto& ele : WorldData)
+	{
+		PlayerInventoryObject->SetNumberField(ele.Key.ToString(), ele.Value);
+	}
+
+	JsonObject->SetObjectField("PlayerInventory", PlayerInventoryObject);
+
+	return JsonObject;
 }
--- Source/MiniBen/Public/Saving/JsonSerializerGISubsystem.h
+++ Source/MiniBen/Public/Saving/JsonSerializerGISubsystem.h
@@ -17,5 +17,13 @@ class MINIBEN_API UJsonSerializerGISubsystem : public UGameInstanceSubsystem
 public:
 	UFUNCTION(BlueprintCallable)
 	void SerializeToJSON(const struct FMainSaveData& data);
+
+protected:
+	TSharedPtr<FJsonObject> SerializeCharacterStats(const struct FCharacterStats& Stats);
+	TSharedPtr<FJsonObject> SerializeSaveableWorldItem(const struct FSaveableWorldItem& Item);
+	TSharedPtr<FJsonObject> SerializeWorldDataSave(const struct FWorldDataSave& WorldData);
+	TSharedPtr<FJsonObject> SerializePlayerInventory(const TMap<FName, int32>& WorldData);
 	
+private:
+	FString FilePath = FPaths::Combine(FPaths::ProjectDir(), TEXT("Saved/SaveGames/MyStructData.json"));
 };
