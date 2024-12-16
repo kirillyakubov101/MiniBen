bacb2056612407bec68d6a73e7bac27c05d623a4
kirill.yakubov <kirillyakubov101@gmail.com>
2024-12-16 00:47:09 -0500

    Complete save/load quests gather
    for gather type quests and sub-quests, there is now save and load system

--- Source/MiniBen/Public/MyStructs.h
+++ Source/MiniBen/Public/MyStructs.h
@@ -135,22 +135,6 @@ public:
 	TMap<FGuid, FSaveableWorldItem> MapOfLevelWorldItems;
 };
 
-USTRUCT(BlueprintType)
-struct FQuestProgress
-{
-	GENERATED_BODY()
-
-public:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	EQuestType QuestType;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	TMap<TSubclassOf<AItem>, int32> TargetsGathered;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	TMap<TSubclassOf<AGameEntity_Enemy>, int32> TargetsKilled;
-};
-
 
 USTRUCT(BlueprintType)
 struct FQuestRequirment
